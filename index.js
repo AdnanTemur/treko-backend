6a2637e16279834912135f152efddc4f50c4f1b3
noumandatu <noumankhan619.915@gmail.com>
2024-07-18 15:34:30 +0500

    text

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ async function startServer() {
     // Establish MongoDB connection
     await connectToDatabase();
 
-    // Start the server
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => {
-      console.log(`⚡ Server is running on port ${port} ⚡`);
-    });
+    // Start the server only if not in production
+    if (process.env.NODE_ENV !== "production") {
+      const port = process.env.PORT || 5000;
+      app.listen(port, () => {
+        console.log(`⚡ Server is running on port ${port} ⚡`);
+      });
+    }
   } catch (error) {
     // Handle MongoDB connection error
     console.error("Error connecting to MongoDB:", error);
   }
 }
 
+// Start the server locally
 startServer();
+
+// Export the app for serverless deployment
+module.exports = app;
